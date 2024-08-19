215f0387759c907243cd53c536c8738f3f15de4c
denaatorabi@gmail.com <denaatorabi@gmail.com>
2024-08-20 00:50:44 +0330

    add

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
 function App() {
  return (
 <Router>
