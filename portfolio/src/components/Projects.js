2eccdbd7498ba75de3d006c62fa589b8af1dffaf
denaatorabi@gmail.com <denaatorabi@gmail.com>
2024-08-21 00:52:53 +0330

    project

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -5,13 +5,32 @@ import React from 'react';
 <h1>Projects</h1>
 <ul className="list-group">
 <li className="list-group-item">
-<a href="https://github.com/yourusername/project1" target="_blank" rel="noopener noreferrer">Project 1</a>
+<a href="https://github.com/Dena-t/MedicalAppointmentSystem-Crud-NodejsReactMySQL.git" target="_blank" rel="noopener noreferrer">Medical Appointmnet System-Crud Project with Node.js React and MySQL.</a>
+<p>This Full stack Crud project uses Node.js for Backend and React for Front end. Enhanced with Authentication and Alerting system for Doctors in case of any changes in appointments</p>
 </li>
 <li className="list-group-item">
-<a href="https://github.com/yourusername/project2" target="_blank" rel="noopener noreferrer">Project 2</a>
+<a href="https://github.com/Dena-t/BookStore-Crud-JavaSpringBoot-MySQL.git" target="_blank" rel="noopener noreferrer">BookStore Project with JavaSpringBoot ,ThymeLeaf and MySql.</a>
+<p>FullStack-Crud Book store Management with Java Spring MVC -Maven- for Backend using REST API and ThymeLeaf, CSS and BootStrap for FrontEnd and Mysql for Database.</p>
 </li>
 <li className="list-group-item">
-<a href="https://github.com/yourusername/project3" target="_blank" rel="noopener noreferrer">Project 3</a>
+<a href="https://github.com/Dena-t/HotelReservationSystem-LaravelMySQL.git" target="_blank" rel="noopener noreferrer">Hotel Resevation System with PHP, Html CSS and MySQL.</a>
+<p>Fullstack Project is created by PHP-Laravel for Backend and uses HTML CSS Twig for front end and Views,and MySQL as Database. </p>
+</li>
+<li className="list-group-item">
+<a href="https://github.com/Dena-t/DogParkFinder-NodejsMongoDB.git" target="_blank" rel="noopener noreferrer">Dog Park Finder Project with Node.js and Mongo DB</a>
+<p>This project uses NodeJs/MongoDB for Business Logic Tier and Javascript/Html Css for front end to perform Crud operations.</p>
+</li>
+<li className="list-group-item">
+<a href="https://github.com/Dena-t/MedClinicManagementSystem-Java.git" target="_blank" rel="noopener noreferrer">Medical Management System with Java(OOP)</a>
+<p>A Medical Management system that uses Java-OOP with Console output in which you can add/remove patients , doctors and treatments.The main purpose of this project is to implement Object orientation concepts in Java.</p>
+</li>
+<li className="list-group-item">
+<a href="https://github.com/Dena-t/CafeWebSiteHtmlCssScss.git" target="_blank" rel="noopener noreferrer">Café website- Html,Css </a>
+<p>A Website for a Café that uses Html , css,Scss , Bootstrap.</p>
+</li>
+<li className="list-group-item">
+<a href="https://github.com/Dena-t/ReforestationProject-HtmlCss.git" target="_blank" rel="noopener noreferrer">Reforestation Project -Html,Css</a>
+<p>A simple Wesbite that uses Html Css for Reforestation.</p>
 </li>
 </ul>
 </div>
